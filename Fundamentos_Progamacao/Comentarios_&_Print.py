60b80bde2e2cd96449f94b2d8b4d490fa97e00c1
Gabriel Carvalho Lorenssetti <gabriel.lorenssetti@softplan.com.br>
2026-02-27 10:58:28 -0300

    update inicio

---
+++ Fundamentos_Progamacao/Comentarios_&_Print.py
@@ -0,0 +1,14 @@
+"""
+    Comentário MultLinha (DocString).
+"""
+
+'''
+    Pode usar simples tbm.
+'''
+
+# Permite escrever um comentario.
+print(123) # Na frente
+# Abaixo
+print(456)
+print(12, 34)
+print(23, 75)
